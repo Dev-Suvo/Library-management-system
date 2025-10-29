c88758f1cd9ff6addf9c3b0b61025aacb2eaa58f
Dev-Suvo <subodipdey20@gmail.com>
2025-10-29 23:25:38 +0530

    login-register page created

--- core/core/urls.py
+++ core/core/urls.py
@@ -31,6 +31,10 @@ urlpatterns = [
 
     path('update/<id>/', update, name='update' ),
     path('delete/<id>/', delete, name='delete' ),
+
+    path('login/', login, name='login' ),
+    path('register/', register, name='register' ),
+
     
 
 ]
--- core/lib/views.py
+++ core/lib/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render , redirect
 from .models import *
+from django.contrib import messages
 # Create your views here.
 
 
@@ -60,3 +61,36 @@ def delete(request, id):
     queryset.delete()
     return redirect('/table/')
 
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    if request.method == "POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+
+
+        user = User.objects.filter(username = username)
+        if user.exists():
+            messages.error(request, 'Username Already Exists!!')
+            return redirect('/register/')
+        
+
+        user = User.objects.create(
+        first_name = first_name,
+        last_name = last_name,
+        username = username,
+        email = email,
+        )
+        user.set_password(password1)
+        user.save()
+        messages.info(request, 'Account Created Successfully')
+
+
+        return redirect('/register')
+
+    return render(request, 'register.html')
